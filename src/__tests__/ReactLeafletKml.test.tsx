67c4524d04955087848337df53f2feab31d16df4
Avi <aviklaiman@hotmail.com>
2021-05-20 19:01:14 +0300

    Increasing test coverage

--- src/__tests__/ReactLeafletKml.test.tsx
+++ src/__tests__/ReactLeafletKml.test.tsx
@@ -4,17 +4,29 @@ import { MapContainer } from 'react-leaflet';
 import ReactLeafletKml from '../index';
 import * as ReactTestUtils from 'react-dom/test-utils';
 
-test('ReactLeafletKml', () => {
-  expect(ReactLeafletKml);
-
-  const dom = ReactTestUtils.renderIntoDocument(
-    <div>
-      <MapContainer>
-        <ReactLeafletKml kml={new Document()}/>
-      </MapContainer>
-    </div>
-  ) as any;
-  const component = ReactDOM.findDOMNode(dom.childNodes[0]) as any;
-  expect(component).toBeInstanceOf(HTMLElement);  
-
+describe('ReactLeafletKml', () => {  
+  it('Draw HTML element', () => {
+    const dom = ReactTestUtils.renderIntoDocument(
+      <div>
+        <MapContainer>
+          <ReactLeafletKml kml={new Document()}/>
+        </MapContainer>
+      </div>
+    ) as any;
+    const component = ReactDOM.findDOMNode(dom.childNodes[0]) as any;
+    expect(component).toBeInstanceOf(HTMLElement);
+  }); 
+  
+  it('MapContainer with preferCanvas', () => {
+    const dom = ReactTestUtils.renderIntoDocument(
+      <div>
+        <MapContainer preferCanvas>
+          <ReactLeafletKml kml={new Document()}/>
+        </MapContainer>
+      </div>
+    ) as any;
+    const component = ReactDOM.findDOMNode(dom.childNodes[0]) as any;
+    expect(component).toBeInstanceOf(HTMLElement);
+  }); 
+  
 });
